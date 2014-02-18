ab1e96f765ca0fb7e8c6b6736d11af915e362bb7
Filip W <wojcieszyn@gmail.com>
2014-02-18 10:10:49 +0100

    added tests

---
+++ test/WebApiContrib.Formatting.ProtoBuf.Test/EvilItem.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiContrib.Formatting.ProtoBuf.Test
+{
+    public class EvilItem
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ test/WebApiContrib.Formatting.ProtoBuf.Test/Item.cs
@@ -0,0 +1,13 @@
+﻿using ProtoBuf;
+
+namespace WebApiContrib.Formatting.ProtoBuf.Test
+{
+    [ProtoContract]
+    public class Item
+    {
+        [ProtoMember(1)]
+        public int Id { get; set; }
+        [ProtoMember(2)]
+        public string Name { get; set; }
+    }
+}
